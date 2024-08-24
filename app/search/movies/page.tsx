33b38db18504f334443d1c7040e3f8ee0c4f23f6
Ali Raza <alirazadev27@gmail.com>
2024-08-24 18:58:07 +0500

    FIXED: deployment error

--- app/search/movies/page.tsx
+++ app/search/movies/page.tsx
@@ -1,18 +1,21 @@
 import { getMovieByTitle } from "@/actions/movieActions";
 import { AddMovieToDB } from "@/components/add_movie_to_db";
+import { auth } from "@/auth";
 
 import { MovieCard } from "@/components/movie_card";
 
 export default async function Page({ searchParams }: { searchParams: any }) {
+  const session: any = await auth();
+  const role = session?.user?.role || "VISITOR";
   const result = await getMovieByTitle(searchParams.query);
+
   return (
     <div className="flex flex-col gap-4 py-4 px-4">
       {result.results.map((movie: any) => (
-        <div className="relative">
-            <MovieCard key={movie.id} movie={movie} />
-            <AddMovieToDB movie={movie} />
+        <div className="relative" key={movie.id}>
+            <MovieCard  movie={movie} />
+            {role === "ADMIN" && <AddMovieToDB movie={movie} />}
         </div>
-
       ))}
     </div>
   );
