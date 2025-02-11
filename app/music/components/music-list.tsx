493d7bfb5e11567af238480f06d528caca3338dd
Ali Raza <alirazadev27@gmail.com>
2025-02-11 10:41:47 +0500

    array length check for no data in musicList

--- app/music/components/music-list.tsx
+++ app/music/components/music-list.tsx
@@ -6,7 +6,6 @@ import { useMusicStore } from "../music-context"
 
 export function MusicList({musicPromise,handleSelection,handleSearch}){
     const result = use<any>(musicPromise)
-    // const musicList = result.music.slice(0,10);
     const musicList = useMusicStore((state:any) => state.music)
     const setMusic = useMusicStore((state:any) => state.addMusic)
     const setFullMusicList = useMusicStore((state:any) => state.setFullMusicList)
@@ -23,7 +22,9 @@ export function MusicList({musicPromise,handleSelection,handleSearch}){
             (entries) => {
                 const lastItem = entries[0]
                 if (lastItem.isIntersecting) {
-                    setVisibleCount((prev) => Math.min(prev + 5, musicList.length)) // Load 5 more at a time
+                  if(musicList?.length && musicList?.length>0){
+                    setVisibleCount((prev) => Math.min(prev + 5, musicList?.length)) // Load 5 more at a time
+                  }
                 }
             },
             { root: null, rootMargin: "100px", threshold: 0.1 }
@@ -41,7 +42,7 @@ export function MusicList({musicPromise,handleSelection,handleSearch}){
     return(
         <Suspense fallback={<div>Loading...</div>}>
           <div id="content" className="w-full md:border-l px-4 flex-1 space-y-4 pt-4  overflow-y-auto overflow-x-clip">
-            {musicList && musicList.length > 0 && musicList.slice(0, visibleCount).map((music: Music, index: number) => (
+            {musicList && musicList?.length > 0 && musicList?.slice(0, visibleCount).map((music: Music, index: number) => (
               <div key={index} className='w-full  flex  items-center gap-4'>
                 <Input type="checkbox" value={music.id} onChange={() => handleSelection(music.id)} className='size-5 shrink-0 selection-box hidden' />
                 <MusicCard  music={music} index={index} handleSearch={handleSearch} />
