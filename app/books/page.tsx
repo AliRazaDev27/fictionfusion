6db0abd2e364d5b2b33b2d73c485f21b5c5203bc
AliRaza <alirazadev27@gmail.com>
2025-07-09 17:39:06 +0500

    Partial book card design

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,34 +1,22 @@
 import BookCard from "@/components/book_card";
-import { auth } from "@/auth";
-import { getBookList } from "@/actions/userListActions";
-import PaginationControll from "@/components/pagination"
-import SearchAndFilter from "@/components/seach_filter_sheet"
 import { Sidebar } from "./components/sidebar";
 import { getBooks } from "@/actions/bookActions";
 
 export default async function Page(props:{searchParams: Promise<any>}) {
   const searchParams = await props.searchParams;
-  const page = Number(searchParams.page) || 1;
-  const search = searchParams.search;
-  const sort = searchParams.sort;
-  const LIMIT = 10;
   const books = await getBooks();
   console.log(books)
   return (
     <div className="relative min-h-[100svh - 70px]">
-      {/* <SearchAndFilter type="books" /> */}
       <div className="flex items-center">
-      <Sidebar />
-      <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 px-4 h-[calc(100vh-70px)] w-full overflow-y-auto">
+      {/* <Sidebar /> */}
+      <section className="p-4 w-full flex flex-col gap-16 items-center border border-white">
         {books &&
         books.map((book, index) => (
             <BookCard key={index} book={book}/>
         ))}
       </section>
       </div>
-    {/* <section className="py-2">
-      <PaginationControll count={result?.total || 0} limit={LIMIT} />
-    </section> */}
     </div>
   );
 }
--- components/book_card.tsx
+++ components/book_card.tsx
@@ -1,14 +1,18 @@
 "use client";
 import Image from "next/image";
-import placeholder from "@/public/bookplaceholder.svg";
 import { getAuthorId, getOpenLibraryAuthorLink, getOpenLibraryCoverLink } from "@/lib";
 import { FaBookOpen } from "react-icons/fa6";
 import { Book } from "@/types";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { IoPersonCircle } from "react-icons/io5";
+import { MdFavoriteBorder, MdMore } from "react-icons/md";
+import { MdDone } from "react-icons/md";
+
+
 
 import Link from "next/link";
 import { Badge } from "./ui/badge";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 export default function BookCard({ book }: { book: Book }) {
 
@@ -16,9 +20,9 @@ export default function BookCard({ book }: { book: Book }) {
   if (!rating) rating = (book as any)?.ratings_average;
   if (!rating) rating = "0"
   return (
-    <div className="relative group/bookcard rounded-2xl border border-white/30 h-min">
+    <div className="2xl:max-w-7xl w-full flex items-center rounded-3xl overflow-hidden shadow-2xl shadow-neutral-600">
       <div
-        className="relative aspect-3/4 rounded-2xl overflow-hidden"
+        className="w-1/3 relative aspect-3/4"
       >
         <Image
           src={getOpenLibraryCoverLink(book.cover_edition_key) || "/bookplaceholder.svg"}
@@ -26,29 +30,26 @@ export default function BookCard({ book }: { book: Book }) {
           quality={100}
           // placeholder="blur"
           // blurDataURL="/bookplaceholder.svg"
-          style={{ objectFit: "cover" }}
+          style={{ objectFit: "cover", objectPosition: "center" }}
           fill
           sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
         />
       </div>
-      <div className="absolute overflow-y-auto top-0 left-0 w-full h-full  scale-0 group-hover/bookcard:scale-100 transition-all duration-500 group-hover/bookcard:flex flex-col gap-4 shadow-md shadow-black px-4 py-4 bg-white/90 rounded-2xl">
-        <div className="flex justify-between items-center">
-          <h1 className="text-xl sm:text-2xl lg:text-xl  font-semibold italic">{book.title}</h1>
+      <div className="relative overflow-y-auto w-full aspect-[2.26] flex flex-col gap-5 p-2 bg-neutral-300">
+        <h1 className="text-2xl  font-extrabold text-neutral-600">{book.title}</h1>
+        <div className="absolute top-3 right-3  flex  gap-2">
+          <button className="p-2 bg-white rounded-full "><MdFavoriteBorder className="size-6" /></button>
+          <button className="p-2 bg-white rounded-full "><MdDone className="size-6" /></button>
         </div>
-        <div className="flex items-center gap-8">
-          <div className="flex gap-2 items-center">
-
-            <div>
-              <Avatar className="size-14">
-                <AvatarImage src={getOpenLibraryAuthorLink("olid", book?.author_key ? book?.author_key[0]: null, "S")} className="object-cover object-center" />
-                <AvatarFallback><IoPersonCircle className="size-14" /></AvatarFallback>
-              </Avatar>
-            </div>
-            {/* TODO: some beautiful animation here */}
-            <Link href={`/author/${getAuthorId(book?.author_key![0])}`} prefetch={false} className="px-2 py-1  hover:bg-blue-600 hover:no-underline hover:rounded-2xl text-base underline underline-offset-4 transition-color duration-500">{book.author_name}</Link>
-          </div>
+        <div className="flex gap-1 items-center">
+          <Avatar className="size-12 md:size-14">
+            <AvatarImage src={getOpenLibraryAuthorLink("olid", book?.author_key ? book?.author_key[0] : null, "M")} className="object-cover object-center" />
+            <AvatarFallback><IoPersonCircle className="size-14" /></AvatarFallback>
+          </Avatar>
+          {/* TODO: some beautiful animation here */}
+          <Link href={`/author/${getAuthorId(book?.author_key![0])}`} prefetch={false} className="px-2 py-1  hover:bg-blue-600 hover:no-underline hover:rounded-2xl text-lg font-semibold underline underline-offset-4 transition-color duration-500">{book.author_name}</Link>
         </div>
-        <div className="flex justify-evenly items-center">
+        <div className="flex gap-8 ps-2 items-center">
           <p className="font-semibold">{book.first_publish_year}</p>
           <div className="flex gap-2 items-center">
             <Badge className="text-sm">{Number(rating).toFixed(2)}</Badge>
@@ -58,12 +59,30 @@ export default function BookCard({ book }: { book: Book }) {
             <FaBookOpen />
           </div>
         </div>
-        <div>
-          {/*  add proper style for description. */}
-          <div className="w-full mt-4">
-              <p className="">{book?.description}</p>
-          </div>
+        {/*  add proper style for description. */}
+        <div className="w-full">
+          <Dialog>
+            <DialogTrigger>
+          <p className="line-clamp-3 text-start cursor-pointer">{book?.description}
+          </p>
+            </DialogTrigger>
+            <DialogContent className="w-[60em]">
+              <DialogTitle>{book.title}</DialogTitle>
+              <DialogDescription className="text-base lg:text-lg font-medium font-sans text-neutral-600">{book?.description}</DialogDescription>
+            </DialogContent>
+          </Dialog>
         </div>
+        {
+          book?.tags && book?.tags.length > 0 && (
+            <div className="flex gap-2">
+              <p>Tags:</p>
+              <div className="flex gap-2 items-center flex-wrap">
+                {book?.tags?.map((tag: any, index: number) => (
+                  <Badge key={index} className="text-sm cursor-pointer">{tag}</Badge>
+                ))}
+              </div>
+            </div>
+          )}
       </div>
     </div>
   );
