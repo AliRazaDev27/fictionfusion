fe32886b650815656f34e8f09bdeebc508ce2ec1
Ali Raza <alirazadev27@gmail.com>
2025-08-14 08:11:17 +0500

    Renamed to /celebs route

--- app/page.tsx
+++ app/page.tsx
@@ -10,28 +10,21 @@ export default function LandingPage() {
           Discover <span className="text-[#f97316]">Organize</span> Track
         </h1>
         <p className="text-xl mb-8 animate-fade-in-up">All your favorite books, movies and shows in one place</p>
-        
-      
-
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-12 w-full max-w-4xl">
           {[
-            { icon: <BookOpen className="h-10 w-10" />, title: "Books", description: "Discover new worlds through literature", linkTo:"/books" },
-            { icon: <Film className="h-10 w-10" />, title: "Movies", description: "Experience cinematic masterpieces", linkTo:"/movies" },
-            { icon: <Tv className="h-10 w-10" />, title: "Shows", description: "Binge-watch your favorite series", linkTo:"/shows" },
+            { icon: <BookOpen className="h-10 w-10" />, title: "Books", description: "Discover new worlds through literature", linkTo: "/books" },
+            { icon: <Film className="h-10 w-10" />, title: "Movies", description: "Experience cinematic masterpieces", linkTo: "/movies" },
+            { icon: <Tv className="h-10 w-10" />, title: "Shows", description: "Binge-watch your favorite series", linkTo: "/shows" },
           ].map((feature, index) => (
-            
-            <FeatureCard 
-              key={feature.title} 
-              {...feature} 
+
+            <FeatureCard
+              key={feature.title}
+              {...feature}
             />
-           
           ))}
         </div>
-
-          <Link className="text-lg font-semibold  text-orange-500 bg-black hover:text-white hover:bg-orange-600 rounded-full px-6 py-3 transition-colors duration-100" href="/login">Get Started</Link>
-
+        <Link className="text-lg font-semibold  text-orange-500 bg-black hover:text-white hover:bg-orange-600 rounded-full px-6 py-3 transition-colors duration-100" href="/login">Get Started</Link>
       </main>
-
       <section className="py-16  backdrop-blur-lg">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-8">Why Choose FictionFusion?</h2>
@@ -49,7 +42,6 @@ export default function LandingPage() {
           </div>
         </div>
       </section>
-
       <footer className="p-4 text-center text-sm">
         <div className="container mx-auto">
           <p>© 2023 <span className="font-semibold text-[#f97316]">FictionFusion</span>. All rights reserved.</p>
@@ -59,12 +51,12 @@ export default function LandingPage() {
   )
 }
 
-function FeatureCard({ icon, title, description, linkTo}: { icon: React.ReactNode; title: string; description: string; linkTo?: string }) {
+function FeatureCard({ icon, title, description, linkTo }: { icon: React.ReactNode; title: string; description: string; linkTo?: string }) {
   return (
     <div className={`flex flex-col items-center p-6 rounded-lg transition-all duration-300 transform bg-[#2a4a9e] hover:bg-[#3a5abe]`}>
       {icon}
-      {linkTo ? <Link href={linkTo} className='text-lg font-semibold  text-orange-500 hover:bg-black hover:text-white rounded-full px-4 py-2 mt-1 transition-colors duration-500'>{title}</Link>: <h2 className="mt-4 text-xl font-semibold">{title}</h2>}
+      {linkTo ? <Link href={linkTo} className='text-lg font-semibold  text-orange-500 hover:bg-black hover:text-white rounded-full px-4 py-2 mt-1 transition-colors duration-500'>{title}</Link> : <h2 className="mt-4 text-xl font-semibold">{title}</h2>}
       <p className="mt-2 text-sm text-neutral-300">{description}</p>
-    </div> 
+    </div>
   )
 }
--- components/nav_links.tsx
+++ components/nav_links.tsx
@@ -18,14 +18,28 @@ export default function NavLinks() {
   return (
     <nav className="hidden text-lg font-medium md:flex md:flex-row md:items-center md:gap-4  lg:gap-4">
 
-      <Link
-        href="/watchlist"
-        prefetch={false}
-        className={cn(`transition-colors duration-100 hover:text-orange-500 text-xl`, pathname === "/watchlist" ? "text-white font-semibold" : "text-white/60 ")}
-      >
-        Watchlist
-      </Link>
 
+      <div className="relative group cursor-pointer px-1 py-3">
+        <p className="text-white/60 text-xl">Track</p>
+        <div className="hidden group-hover:block absolute left-1/2 -translate-x-1/2 z-50 bg-linear-to-b from-gray-950 to-blue-950 border border-white/50 px-6 py-6 mt-2 rounded-lg">
+          <div className="flex flex-col items-center gap-4">
+            <Link
+              href="/watchlist"
+              prefetch={false}
+              className={cn(`transition-colors duration-100 hover:text-orange-500 text-xl`, pathname === "/watchlist" ? "text-white font-semibold" : "text-white/60 ")}
+            >
+              Watchlist
+            </Link>
+            <Link
+              href="/celebs"
+              prefetch={false}
+              className={cn(`transition-colors duration-100 hover:text-orange-500 text-xl`, pathname === "/celebs" ? "text-white font-semibold" : "text-white/60 ")}
+            >
+              Celebs
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className="relative group cursor-pointer px-1 py-3">
         <p className="text-white/60 text-xl">Explore</p>
         <div className="hidden group-hover:block absolute left-1/2 -translate-x-1/2 z-50 bg-linear-to-b from-gray-950 to-blue-950 border border-white/50 px-6 py-6 mt-2 rounded-lg">
@@ -53,7 +67,7 @@ export default function NavLinks() {
             <AddTask />
             {/* VIEW */}
             <ViewTasks />
-            </div>
+          </div>
         </div>
       </div>
 
