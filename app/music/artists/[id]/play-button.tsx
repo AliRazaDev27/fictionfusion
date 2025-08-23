337a48037f579ffc0cfc17d4708e86769959f209
Ali Raza <alirazadev27@gmail.com>
2025-08-23 10:41:33 +0500

    Implemented Get all songs by artist feature

---
+++ app/music/artists/[id]/play-button.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { useRef, useState } from "react";
+
+export function PlayButton({ src }: { src: string }) {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const [playing, setPlaying] = useState(false);
+
+  const togglePlay = () => {
+    if (!audioRef.current) return;
+    if (playing) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    setPlaying(!playing);
+  };
+
+  return (
+    <div className="me-2">
+      <button className="text-2xl cursor-pointer" onClick={togglePlay}>
+        {playing ? "⏸️" : "▶️"}
+      </button>
+      <audio ref={audioRef} src={src} />
+    </div>
+  );
+}
--- app/music/music-card.tsx
+++ app/music/music-card.tsx
@@ -2,6 +2,8 @@
 import { useEffect, forwardRef, useState } from "react";
 import { Music } from "@/lib/database/musicSchema";
 import { useMusicStore } from "./music-context";
+import Link from "next/link";
+import { ExternalLink } from "lucide-react";
 
 
 interface MusicCardProps {
@@ -36,14 +38,21 @@ export const MusicCard = forwardRef<HTMLDivElement, MusicCardProps>(({ music, in
   }, [music]);
 
   return (
-    <div ref={ref} id={`music-card-${index}`} className='w-full flex  gap-2 overflow-hidden text-white font-semibold text-xl px-2 py-2  hover:bg-teal-700  bg-slate-800 cursor-pointer rounded-xl' style={{ backgroundColor: index === currentMusic ? "rgba(0, 122, 255, 1)" : "" }} key={music.id} onClick={selectMusic}>
+    <div ref={ref} id={`music-card-${index}`} className='w-full flex  gap-2 overflow-hidden text-white font-semibold text-xl px-2 py-2  hover:bg-teal-700  bg-slate-800 cursor-pointer rounded-xl' style={{ backgroundColor: index === currentMusic ? "rgba(0, 122, 140, 1)" : "" }} key={music.id} onClick={selectMusic}>
       <div className='relative z-0 shrink-0 w-[100px] h-[100px] rounded-lg overflow-hidden'>
         <img src={music.coverArt || `/music-player.png`} loading="lazy" alt="cover-art" className='w-full h-full object-cover' width={100} height={100} />
         {cached && <span className="absolute bottom-1 right-1 rounded-full w-3 h-3 border bg-lime-400"/>}
       </div>
       <div className='flex pt-1 ps-1 flex-col gap-1'>
         <p className="text-lg md:text-xl">{music.title}</p>
-        <p className="text-sm md:text-base text-gray-300">{music.artist || "Unknown"}</p>
+        <Link 
+        href={`/music/artists/${encodeURIComponent(music.artist)}`} 
+        prefetch={false} 
+        target="_blank" 
+        className="flex items-center gap-1 text-sm md:text-base text-gray-300 hover:text-white transition">
+          {music.artist || "Unknown"}
+          <ExternalLink size={14} />
+          </Link>
       </div>
     </div>
   )
