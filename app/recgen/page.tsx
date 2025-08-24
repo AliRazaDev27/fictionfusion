106138c1872e8ab67e096f122dae24ee2e394343
Ali Raza <alirazadev27@gmail.com>
2025-08-24 17:58:49 +0500

    skelton page for recgen

---
+++ app/recgen/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page(){
+  return(
+    <main className="flex min-h-[90vh] flex-col items-center justify-center text-white">
+    <h1 className="text-2xl">RecGen</h1>
+    <p>Will implement later</p>
+    </main>
+  )
+}
