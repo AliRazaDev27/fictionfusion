daa3dbbf71a8e217fddf4aa3d020218ca4a5909c
Ali Raza <alirazadev27@gmail.com>
2025-08-29 17:34:27 +0500

    minor styling changes for /watchlist card.

--- components/mydramaShowCard.tsx
+++ components/mydramaShowCard.tsx
@@ -1,13 +1,15 @@
 "use client";
+import { useRef, useState } from "react";
 import Image from "next/image";
-import { addItemToIgnoreList } from "@/actions/ignorelistActions";
 import { useToast } from "./ui/use-toast";
-import { useRef } from "react";
-import {  ShowMyDramalist } from "@/types";
+import { ShowMyDramalist } from "@/types";
 import { addMyDramalistShow } from "@/actions/watchlistActions";
-
+import { addItemToIgnoreList } from "@/actions/ignorelistActions";
+import { MdClose, MdDescription, MdSave } from "react-icons/md";
+import { TiCancel } from "react-icons/ti";
 
 export default function MyDramaShowCard({ item }: { item: ShowMyDramalist }) {
+  const [showDescription,setShowDescription] = useState(false);
   const cardRef = useRef<HTMLDivElement>(null);
   const { toast } = useToast()
   const handleClick = async () => {
@@ -60,16 +62,18 @@ export default function MyDramaShowCard({ item }: { item: ShowMyDramalist }) {
     }
   }
   return (
-    <div ref={cardRef} className="bg-sky-950 relative flex flex-col gap-3 items-center justify-between rounded-lg  px-2 py-3 text-center overflow-hidden transition-all duration-500"
-      style={{ background: 'linear-gradient(220.55deg, #B9A14C 0%, #000000 100%)' }}>
+    <div
+      ref={cardRef}
+      className="bg-teal-950 relative flex flex-col gap-1 items-center justify-between rounded-lg  px-2 py-3 text-center overflow-hidden transition-all duration-500"
+      >
       <div className="relative overflow-hidden w-[90%]  mx-auto aspect-2/3">
         <Image src={item?.image || ""} alt="cover" fill quality={100} className="bg-cover" />
       </div>
       {
-        item.ranking && <div className="absolute top-0 right-0 text-lg text-white bg-orange-500 px-4 py-2 rounded-bl-3xl">{item.ranking}</div>
+        item.ranking && <div className="absolute top-0 right-0  text-white bg-cyan-700 px-4 py-2 rounded-bl-3xl">{item.ranking}</div>
       }
       {
-        item.rating && <div className="absolute top-0 left-0 text-lg text-white bg-orange-500 px-4 py-2 rounded-br-3xl">{item.rating}</div>
+        item.rating && <div className="absolute top-0 left-0  text-white bg-cyan-700 px-4 py-2 rounded-br-3xl">{item.rating}</div>
       }
       <a
         href={`https://mydramalist.com${item.link}`}
@@ -82,17 +86,28 @@ export default function MyDramaShowCard({ item }: { item: ShowMyDramalist }) {
         item?.info && <div className="text-neutral-300">{item?.info}</div>
       }
       {
-        item?.description && <div className="text-white text-lg line-clamp-4">{item?.description}</div>
-      }
-      <div className="flex gap-4">
-        <button className="bg-black px-4 py-2 text-white rounded-xl hover:bg-orange-600 cursor-pointer" onClick={handleClick}>
-          Ignore
+        item?.description && showDescription &&
+        <div 
+        className="absolute inset-0 w-full h-full flex items-center justify-center bg-black/90 ">
+        <button onClick={() => setShowDescription(false)}>
+          <MdClose className="bg-red-600 text-white rounded-full w-8 h-8 p-1 cursor-pointer absolute top-4 right-4"/>
         </button>
-        {/* <AddShowToDB show={showObject}/> */}
-        <button className="bg-black px-4 py-2 text-white rounded-xl hover:bg-orange-600 cursor-pointer" onClick={saveToDB}>
-          Add to DB
+        <p className="text-white text-lg md:text-xl ">{item?.description}</p>
+          </div>
+      }
+      
+      <div className="flex items-center justify-between w-full gap-4 text-lg">
+        <button className="bg-black px-4 py-2 text-white rounded-lg hover:bg-orange-600 cursor-pointer" onClick={() => setShowDescription(true)}>
+          <MdDescription />
+        </button>
+        <button className="bg-black px-4 py-2 text-white rounded-lg hover:bg-orange-600 cursor-pointer" onClick={handleClick}>
+          <TiCancel />
+        </button>
+        <button className="bg-black px-4 py-2 text-white rounded-lg hover:bg-orange-600 cursor-pointer" onClick={saveToDB}>
+          <MdSave />
         </button>
       </div>
+
     </div>
   )
 }
