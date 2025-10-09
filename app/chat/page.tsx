4fa74f8621cc9ce0613a07440d41ef9da4d692ad
Ali Raza <alirazadev27@gmail.com>
2025-10-09 20:39:35 +0500

    streamText working, need more info on what it returns each iteration of streamableText.

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,36 +1,47 @@
 "use client";
-import { sendMessage } from "@/actions/chatActions";
+import "./styles.css";
+import { streamMessage } from "@/actions/chatActions";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, Suspense, useRef } from "react";
 import { readStreamableValue } from '@ai-sdk/rsc';
 
+export const maxDuration = 30;
 export default function Page() {
-    const [content,setContent] = useState<string>("");
-    const handleSend = async (message:string) => {
+    const generation  = useRef<string>('');
+    const textcontent = useRef<HTMLTextAreaElement>(null);
+    const handleSend = async (message: string) => {
         const start = performance.now();
         console.log(message)
-        const response = await sendMessage(message);
+        const { output,success } = await streamMessage(message, "gemini-2.5-flash");
+        if(!success || !output) return;
+        for await (const delta of readStreamableValue(output)) {
         console.log("response time", (performance.now() - start) / 1000);
-        setContent(response);
+            generation.current = `${generation.current}${delta}`;
+            if (textcontent.current) {
+                textcontent.current.value = generation.current;
+            }
+        }
 
     }
     return (
-        <div className="flex flex-col w-full h-[89svh] bg-gray-800 text-white/90">
-            <div className="flex-1 overflow-y-auto overflow-hidden w-full p-8">
-                <pre className="w-full text-lg  text-wrap">{content}</pre>
-            </div>
-            <div className="mt-auto">
-                <Input 
-                className="w-2/3 mx-auto text-black"
-                 placeholder="Type something..."
-                 // on enter send
-                 onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleSend(e.currentTarget.value);
-                      e.currentTarget.value = "";
-                    }
-                  }}
-                 ></Input>
+        <div className="flex flex-col gap-2 w-full p-2 min-h-[calc(100vh-70px)] bg-gray-900 text-white/90">
+                <textarea id='chat' ref={textcontent}
+                 className="w-full h-[72dvh] text-lg  text-wrap prose prose-invert prose-neutral bg-gray-950
+            flex-1 border border-neutral-400 rounded-xl overflow-y-auto max-w-3xl 
+            mx-auto p-4 leading-relaxed text-[20px] text-neutral-300 font-serif">
+                </textarea>
+            <div className="mt-auto max-w-3xl mx-auto w-full">
+                <Input
+                    className="p-3 md:p-6 md:text-xl  text-black"
+                    placeholder="Type something..."
+                    // on enter send
+                    onKeyDown={(e) => {
+                        if (e.key === "Enter") {
+                            handleSend(e.currentTarget.value);
+                            e.currentTarget.value = "";
+                        }
+                    }}
+                ></Input>
             </div>
         </div>
     );
